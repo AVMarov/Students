95f95a8860d4ec2c063a5fb86d92a03fbf18b97d
Ilya Chikmarev <ichikmarev@gmail.com>
2020-04-02 21:05:47 +0300

    Review

--- Students/ViewController.swift
+++ Students/ViewController.swift
@@ -15,35 +15,36 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
     
     //Студенты, все персонажи вымышлены любые совпадения случайны
     let students = [
-    Student(name: "Иван", surname: "Иванов", age: 18, rating: 10),
-    Student(name: "Петр", surname: "Смирнов", age: 19, rating: 9),
-    Student(name: "Алексей", surname: "Сидоров", age: 20, rating: 8),
-    Student(name: "Александр", surname: "Петров", age: 21, rating: 7),
-    Student(name: "Владимир", surname: "Кузнецов", age: 22, rating: 6),
-    Student(name: "Петр", surname: "Иванов", age: 23, rating: 5),
-    Student(name: "Владимир", surname: "Смирнов", age: 24, rating: 4),
-    Student(name: "Александр", surname: "Сидоров", age: 25, rating: 9),
-    Student(name: "Алексей", surname: "Петров", age: 26, rating: 6),
-    Student(name: "Иван", surname: "Кузнецов", age: 27, rating: 7),
-    Student(name: "Константин", surname: "Соколов", age: 28, rating: 10),
-    Student(name: "Андрей", surname: "Михайлов", age: 29, rating: 9),
-    Student(name: "Дмитрий", surname: "Егоров", age: 30, rating: 8),
-    Student(name: "Денис", surname: "Орлов", age: 21, rating: 7),
-    Student(name: "Борис", surname: "Лебедев", age: 22, rating: 6),
-    Student(name: "Олег", surname: "Соколов", age: 23, rating: 5),
-    Student(name: "Глеб", surname: "Михайлов", age: 24, rating: 4),
-    Student(name: "Егор", surname: "Егоров", age: 25, rating: 9),
-    Student(name: "Константин", surname: "Орлов", age: 26, rating: 6),
-    Student(name: "Андерей", surname: "Лебедев", age: 27, rating: 7)
-    ].sorted(by: {$1.getFullName() > $0.getFullName()})
+        Student(name: "Иван", surname: "Иванов", age: 18, rating: 10),
+        Student(name: "Петр", surname: "Смирнов", age: 19, rating: 9),
+        Student(name: "Алексей", surname: "Сидоров", age: 20, rating: 8),
+        Student(name: "Александр", surname: "Петров", age: 21, rating: 7),
+        Student(name: "Владимир", surname: "Кузнецов", age: 22, rating: 6),
+        Student(name: "Петр", surname: "Иванов", age: 23, rating: 5),
+        Student(name: "Владимир", surname: "Смирнов", age: 24, rating: 4),
+        Student(name: "Александр", surname: "Сидоров", age: 25, rating: 9),
+        Student(name: "Алексей", surname: "Петров", age: 26, rating: 6),
+        Student(name: "Иван", surname: "Кузнецов", age: 27, rating: 7),
+        Student(name: "Константин", surname: "Соколов", age: 28, rating: 10),
+        Student(name: "Андрей", surname: "Михайлов", age: 29, rating: 9),
+        Student(name: "Дмитрий", surname: "Егоров", age: 30, rating: 8),
+        Student(name: "Денис", surname: "Орлов", age: 21, rating: 7),
+        Student(name: "Борис", surname: "Лебедев", age: 22, rating: 6),
+        Student(name: "Олег", surname: "Соколов", age: 23, rating: 5),
+        Student(name: "Глеб", surname: "Михайлов", age: 24, rating: 4),
+        Student(name: "Егор", surname: "Егоров", age: 25, rating: 9),
+        Student(name: "Константин", surname: "Орлов", age: 26, rating: 6),
+        Student(name: "Андерей", surname: "Лебедев", age: 27, rating: 7)
+        ].sorted(by: { $1.getFullName() > $0.getFullName() })
     // Сортировка по полному имени, если только по фамилии, то имена в случайном порядке
     
-    var filteredData: [Student]!
+    var filteredData = [Student]() //Don't need to force unwrap here
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self        
         searchBar.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "StudentCell") // Need to register every reuse indetifier of cell in tableview
         filteredData = students
     }
     
@@ -57,17 +58,14 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
         return cell
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
-        
-        filteredData = searchText.isEmpty ? students : students.filter { (item: Student) -> Bool in
-            return item.getFullName().range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? students : students.filter {
+            $0.fullname.range(of: searchText,
+                              options: .caseInsensitive,
+                              range: nil,
+                              locale: nil) != nil //Better like this
         }
         
         tableView.reloadData()
     }
-    
-    
-
-
 }
-
