15363337cf4da351bb5b3fe55ad793a09965212b
Artyom <MarowArtem@yandex.ru>
2020-04-02 16:16:13 +0300

    Create struct Student, create local instances, add tableview

--- Students/ViewController.swift
+++ Students/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let students = [
+    Students(name: "Иван", surname: "Иванов", age: 18, rating: 10),
+    Students(name: "Петр", surname: "Смирнов", age: 19, rating: 9),
+    Students(name: "Алексей", surname: "Сидоров", age: 20, rating: 8),
+    Students(name: "Александр", surname: "Петров", age: 21, rating: 7),
+    Students(name: "Владимир", surname: "Кузнецов", age: 22, rating: 6),
+    Students(name: "Петр", surname: "Иванов", age: 23, rating: 5),
+    Students(name: "Владимир", surname: "Смирнов", age: 24, rating: 4),
+    Students(name: "Александр", surname: "Сидоров", age: 25, rating: 9),
+    Students(name: "Алексей", surname: "Петров", age: 26, rating: 6),
+    Students(name: "Иван", surname: "Кузнецов", age: 27, rating: 7),
+ 
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
