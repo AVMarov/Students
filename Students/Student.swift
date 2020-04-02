f03ece782856a788757bc5c8d121583398b8aef1
Artyom <MarowArtem@yandex.ru>
2020-04-02 19:29:30 +0300

    Add searchBar and search method

--- Students/Student.swift
+++ Students/Student.swift
@@ -5,9 +5,13 @@
 //  Created by 1 on 02.04.2020.
 //  Copyright © 2020 example. All rights reserved.
 //
-
-import UIKit
-
-class Student: NSObject {
-
+struct Student {
+    var name: String
+    var surname: String
+    var age: Int
+    var rating: Int
+    
+    func getFullName() -> String{
+        return "\(surname) \(name)"
+    }
 }
--- Students/ViewController.swift
+++ Students/ViewController.swift
@@ -8,54 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     //Студенты, все персонажи вымышлены любые совпадения случайны
-    var students = [
-    Students(name: "Иван", surname: "Иванов", age: 18, rating: 10),
-    Students(name: "Петр", surname: "Смирнов", age: 19, rating: 9),
-    Students(name: "Алексей", surname: "Сидоров", age: 20, rating: 8),
-    Students(name: "Александр", surname: "Петров", age: 21, rating: 7),
-    Students(name: "Владимир", surname: "Кузнецов", age: 22, rating: 6),
-    Students(name: "Петр", surname: "Иванов", age: 23, rating: 5),
-    Students(name: "Владимир", surname: "Смирнов", age: 24, rating: 4),
-    Students(name: "Александр", surname: "Сидоров", age: 25, rating: 9),
-    Students(name: "Алексей", surname: "Петров", age: 26, rating: 6),
-    Students(name: "Иван", surname: "Кузнецов", age: 27, rating: 7),
-    Students(name: "Константин", surname: "Соколов", age: 28, rating: 10),
-    Students(name: "Андрей", surname: "Михайлов", age: 29, rating: 9),
-    Students(name: "Дмитрий", surname: "Егоров", age: 30, rating: 8),
-    Students(name: "Денис", surname: "Орлов", age: 21, rating: 7),
-    Students(name: "Борис", surname: "Лебедев", age: 22, rating: 6),
-    Students(name: "Олег", surname: "Соколов", age: 23, rating: 5),
-    Students(name: "Глеб", surname: "Михайлов", age: 24, rating: 4),
-    Students(name: "Егор", surname: "Егоров", age: 25, rating: 9),
-    Students(name: "Константин", surname: "Орлов", age: 26, rating: 6),
-    Students(name: "Андерей", surname: "Лебедев", age: 27, rating: 7)
-    ]
-
-    @IBOutlet weak var tableView: UITableView!
+    let students = [
+    Student(name: "Иван", surname: "Иванов", age: 18, rating: 10),
+    Student(name: "Петр", surname: "Смирнов", age: 19, rating: 9),
+    Student(name: "Алексей", surname: "Сидоров", age: 20, rating: 8),
+    Student(name: "Александр", surname: "Петров", age: 21, rating: 7),
+    Student(name: "Владимир", surname: "Кузнецов", age: 22, rating: 6),
+    Student(name: "Петр", surname: "Иванов", age: 23, rating: 5),
+    Student(name: "Владимир", surname: "Смирнов", age: 24, rating: 4),
+    Student(name: "Александр", surname: "Сидоров", age: 25, rating: 9),
+    Student(name: "Алексей", surname: "Петров", age: 26, rating: 6),
+    Student(name: "Иван", surname: "Кузнецов", age: 27, rating: 7),
+    Student(name: "Константин", surname: "Соколов", age: 28, rating: 10),
+    Student(name: "Андрей", surname: "Михайлов", age: 29, rating: 9),
+    Student(name: "Дмитрий", surname: "Егоров", age: 30, rating: 8),
+    Student(name: "Денис", surname: "Орлов", age: 21, rating: 7),
+    Student(name: "Борис", surname: "Лебедев", age: 22, rating: 6),
+    Student(name: "Олег", surname: "Соколов", age: 23, rating: 5),
+    Student(name: "Глеб", surname: "Михайлов", age: 24, rating: 4),
+    Student(name: "Егор", surname: "Егоров", age: 25, rating: 9),
+    Student(name: "Константин", surname: "Орлов", age: 26, rating: 6),
+    Student(name: "Андерей", surname: "Лебедев", age: 27, rating: 7)
+    ].sorted(by: {$1.getFullName() > $0.getFullName()})
+    // Сортировка по полному имени, если только по фамилии, то имена в случайном порядке
+    
+    var filteredData: [Student]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self        
+        searchBar.delegate = self
+        filteredData = students
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return students.count
+        return filteredData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
-        cell.textLabel?.text = students[indexPath.row].getFullName()
+        cell.textLabel?.text = filteredData[indexPath.row].getFullName()
         return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
         
-        // Сортировка по полному имени, если только по фамилии, то имена в случайном порядке
-        students.sort(by: {$1.getFullName() > $0.getFullName()})
-        tableView.delegate = self
-        tableView.dataSource = self
+        filteredData = searchText.isEmpty ? students : students.filter { (item: Student) -> Bool in
+            return item.getFullName().range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
         
-        // Do any additional setup after loading the view.
+        tableView.reloadData()
     }
+    
+    
 
 
 }
