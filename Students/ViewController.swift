bc42de82cf538e7e6dbdc4ef3e299013ce8a6a60
Artyom <MarowArtem@yandex.ru>
2020-04-02 12:18:54 +0300

    Initial Commit

---
+++ Students/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Students
+//
+//  Created by 1 on 02.04.2020.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
