b102f2a80c62becbe814f1295d54ba5132a87e36
Artyom <MarowArtem@yandex.ru>
2020-04-05 18:39:45 +0300

    Revert "Merge branch 'development'"
    This reverts commit 8629f76ab59deaceeb997df76e3dfc3a20d671c9, reversing
    changes made to f173feb26e6866098e2beda9f38a5c9144b0ccba.

--- Students/Student.swift
+++ Students/Student.swift
@@ -8,25 +8,16 @@
 struct Student {
     var name: String
     var surname: String
-    var gender: String
-    var profile: String?
+    var age: Int
+    var rating: Int
     
-    var age: Int {
-        return Int.random(in: 18...30)
-    }
-    
-    var rating: Double {
-        return Double.random(in: 3...5)
-    }
-    
-    var fullname: String {
+    func getFullName() -> String{
         return "\(surname) \(name)"
     }
-    
-    init(name: String, surname: String, gender: String, profile: String? = nil){
-        self.name = name
-        self.surname = surname
-        self.gender = gender
-        self.profile = profile
+
+    //Better to use computed property
+
+    var fullname: String {
+        return "\(surname) \(name)"
     }
 }
